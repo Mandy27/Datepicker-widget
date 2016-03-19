b50973da3b06191beff786a4b999e46f5cbbcee9
Mandy27 <giaman27@gmail.com>
2016-03-18 23:48:19 -0700

    Successfully implement calendar

--- public/javascripts/Datepicker.js
+++ public/javascripts/Datepicker.js
@@ -12,7 +12,6 @@ function Datepicker(x) {
 
 Datepicker.prototype.render = function() {
 	var self = this;
-
 	var parent = $("#"+self.parent_id);
 	var $div1 = $("<div class='datePicker'></div>");
 	parent.append($div1);
@@ -22,41 +21,27 @@ Datepicker.prototype.render = function() {
 
     // Create Calendar
     var $div3 = $("<div class='calendar'></div>");
-    var $div4 = $("<div class='header month'>"+self._monthList[self._currMonth]+" 2015</div>");
-    var $leftbtn = $("<button type='button' class='btn btn-default header'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span></button>");
-    var $rightbtn = $("<button type='button' class='btn btn-default header'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button>");
+    var $div4 = $("<div class='header month'>"+self._monthList[self._month]+" "+self._year+"</div>");
+    var $leftbtn = $("<button type='button' class='btn btn-default header'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span></button>")
+    				.on("click", function(){
+    					self._month--;
+    					if(self._month < 0) self._month = 11;
+    					updateCalendarHeader();
+    					constructCalendarTable($calBody);
+    				});
+    var $rightbtn = $("<button type='button' class='btn btn-default header'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button>")
+    				.on("click", function(){
+    					self._month++;
+    					if(self._month == 12) self._month = 0;
+    					updateCalendarHeader();
+    					constructCalendarTable($calBody);
+    				});
+    var $calBody = $("<div id='calendar-body'></div>");
     $div3.append($leftbtn);
     $div3.append($div4);
    	$div3.append($rightbtn);
-
-   	//Header row
-   	var $calTable = $("<table width='100%'></table>");
-   	var $tr = $("<tr></tr>");
-   	for(var i = 0; i < self._dayList.length; i++){
-   		$tr.append($("<th>"+self._dayList[i]+"</th>"));
-   	}
-   	$calTable.append($tr);
-
-   	// Body table
-   	var firstday = new Date(this._currYear, this._currMonth, 1);
-   	var endday = new Date(this._currYear, this._currMonth + 1, 1);
-	var monthLength = Math.ceil((endday - firstday) / (1000 * 60 * 60 * 24));
-	var day = 1;
-	for(var i = 0; i < 6; i++){
-		if(day > monthLength) break;
-		var $tr = $("<tr></tr>")
-		for(var j = 0; j < 7; j++){
-			var $td = $("<td></td>");
-			if(day <= monthLength && ( j>= firstday.getDay() || i != 0)){
-				$td.text(day);
-				day++;
-			}
-			$tr.append($td);
-		}
-		$calTable.append($tr);
-	}
-
-	$div3.append($calTable);
+	$div3.append($calBody);
+	constructCalendarTable($calBody);
 
    	$div1.append($div3);
 
@@ -80,4 +65,38 @@ Datepicker.prototype.render = function() {
     		.addClass("caret");
    	$btn.append($span2);
 
+   	function updateCalendarHeader(){
+   		$(".calendar .month").text(self._monthList[self._month] +" "+ self._year);
+   	}
+
+   	function constructCalendarTable(parent){
+   		$("#calendar-body").children().remove();
+   		//Header row
+	   	var $calTable = $("<table width='100%'></table>");
+	   	var $tr = $("<tr></tr>");
+	   	for(var i = 0; i < self._dayList.length; i++){
+	   		$tr.append($("<th>"+self._dayList[i]+"</th>"));
+	   	}
+	   	$calTable.append($tr);
+
+	   	// Body table
+	   	var firstday = new Date(self._year, self._month, 1);
+	   	var endday = new Date(self._year, self._month + 1, 1);
+		var monthLength = Math.ceil((endday - firstday) / (1000 * 60 * 60 * 24));
+		var day = 1;
+		for(var i = 0; i < 6; i++){
+			if(day > monthLength) break;
+			var $tr = $("<tr></tr>")
+			for(var j = 0; j < 7; j++){
+				var $td = $("<td></td>");
+				if(day <= monthLength && ( j>= firstday.getDay() || i != 0)){
+					$td.text(day);
+					day++;
+				}
+				$tr.append($td);
+			}
+			$calTable.append($tr);
+		}
+		parent.append($calTable);
+   	}
 };
